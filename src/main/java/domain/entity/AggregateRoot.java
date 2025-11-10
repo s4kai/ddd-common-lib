3056a75134a0b042b23c32dd5d725c5525237e13
Vinicius Sakai <vinicius.sakai@tuta.io>
2025-11-10 19:24:58 -0300

    feat: add publish all aggregate events

--- src/main/java/domain/entity/AggregateRoot.java
+++ src/main/java/domain/entity/AggregateRoot.java
@@ -1,24 +1,22 @@
 package domain.entity;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
+import domain.events.DomainEvent;
+import lombok.Getter;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
 public abstract class AggregateRoot<ID> extends BaseEntity<ID> {
-    private final List<Object> domainEvents = new CopyOnWriteArrayList<>();
+    private final List<DomainEvent> domainEvents = new ArrayList<>();
 
     protected AggregateRoot(ID id) {
         super(id);
     }
 
-    public void addDomainEvent(Object event) {
+    public void addDomainEvent(DomainEvent event) {
         domainEvents.add(event);
     }
-
-    public List<Object> getDomainEvents() {
-        return Collections.unmodifiableList(domainEvents);
-    }
-
     public void clearDomainEvents() {
         domainEvents.clear();
     }
--- src/main/java/domain/events/DomainEventPublisher.java
+++ src/main/java/domain/events/DomainEventPublisher.java
@@ -1,5 +1,8 @@
 package domain.events;
 
+import domain.entity.AggregateRoot;
+
 public interface DomainEventPublisher {
     void publish(DomainEvent event);
+    void publish(AggregateRoot<?> aggregateRoot);
 }
